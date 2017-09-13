d8cdbd531c236479c9582a1a899850f7a972df09
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-09-12 18:46:18 -0700

    initial commit

---
+++ solid_fill_pdf.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+import os
+import sys
+import csv
+from fdfgen import forge_fdf
+
+
+key_value_tuples = []
+
+
+def fin_translation(fin_value):
+    if fin_value == '1':
+        return ('single', 'X')
+    elif fin_value == '2':
+        return ('twin', 'X')
+    elif fin_value == '3':
+        return ('thruster', 'X')
+    elif fin_value == '4':
+        return ('quad', 'X')
+    elif fin_value == '5':
+        return ('fivefin', 'X')
+    else:
+        return ('otherfin', 'X')
+
+
+def append_fin(header, value):
+    fin_tuple = fin_translation(value)
+    if fin_tuple[0] == 'otherfin':
+        key_value_tuples.append(fin_tuple)
+        print(header, value)
+        key_value_tuples.append((header, value))
+    else:
+        key_value_tuples.append(fin_tuple)
+
+
+def tail_translation(tail_value):
+    if tail_value == 'square':
+        return ('square', 'X')
+    elif tail_value == 'squash':
+        return ('squash', 'X')
+    elif tail_value == 'round':
+        return ('round', 'X')
+    elif tail_value == 'round pin':
+        return ('round pin', 'X')
+    elif tail_value == 'pin':
+       return ('pin', 'X')
+    elif tail_value == 'swallow':
+        return ('swallow', 'X')
+    elif tail_value == 'fish':
+        return ('fish', 'X')
+    else:
+        return ('tail', tail_value)
+
+
+def type_translation(type_value):
+    if type_value == 'future':
+        return ('future', 'X')
+    elif type_value == 'fcs2':
+        return ('fcstwo', 'X')
+    elif type_value == 'fcsii':
+        return ('fcstwo', 'X')
+    elif type_value == 'glasson':
+        return ('glasson', 'X')
+    else:
+        return ('othertype', 'X')
+
+
+def process_csv(csv_file):
+    csv_data = csv.reader(open(csv_file))
+    header = []
+    output_data = []
+    for row_number, row in enumerate(csv_data):
+        if row_number == 0:
+            continue
+        if row_number == 1:
+            header = row
+            header = [word.lower().strip() for word in header]
+            continue
+
+        for i in range(len(header)):
+            value = row[i].strip()
+            if header[i] == 'fins':
+                append_fin(header[i], value)
+            elif header[i] == 'tail':
+                tail_tuple = tail_translation(value.lower())
+                key_value_tuples.append(tail_tuple)
+            elif header[i] == 'or':
+                if value:
+                    key_value_tuples.append(('or', 'X'))
+                    key_value_tuples.append(('ortext', value))
+            else:
+                key_value_tuples.append((header, value))
+        output_data.append(key_value_tuples)
+    return output_data
+
+
+def fill_pdf_template(row, pdf_template, output_file):
+    tmp_file = "tmp.fdf"
+    fdf = forge_fdf("", row, [], [], [])
+    with open(tmp_file, "wb") as fdf_file:
+        fdf_file.write(fdf)
+    cmd = "pdftk '{0}' fill_form '{1}' output '{2}' dont_ask".format(pdf_template, tmp_file, output_file)
+    os.system(cmd)
+    os.remove(tmp_file)
+
+
+def main(args):
+    """
+    Batch fill pdf template with data from csv file
+    """
+    try:
+        pdf_template = sys.argv[1]
+        csv_file = sys.argv[2]
+    except IndexError:
+        print("python3 fill.py pdf_template csv_file")
+    else:
+        data = process_csv(csv_file)
+
+        output_dir = os.path.join(os.getcwd(), 'output')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        for row in data:
+            orderid = row[3][1]
+            output_path = os.path.join(output_dir, orderid)
+            output_file = output_path + ".pdf"
+            fill_pdf_template(row, pdf_template, output_file)
+            print("Generated {0}.pdf".format(orderid))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ test_solid_fill_pdf.py
@@ -0,0 +1,70 @@
+import pytest
+from solid_fill_pdf import fin_translation
+from solid_fill_pdf import tail_translation
+from solid_fill_pdf import type_translation
+from solid_fill_pdf import append_fin
+
+
+def test_single_fin():
+    assert ('single', 'X') == fin_translation('1')
+
+def test_twin_fin():
+    assert ('twin', 'X') == fin_translation('2')
+
+def test_thruster_fin():
+    assert ('thruster', 'X') == fin_translation('3')
+
+def test_quad_fin():
+    assert ('quad', 'X') == fin_translation('4')
+
+def test_fivefin_fin():
+    assert ('fivefin', 'X') == fin_translation('5')
+
+def test_other_fin():
+    assert ('otherfin', 'X') == fin_translation('2+1')
+
+def test_other_fin():
+    assert ('otherfin', 'X') == fin_translation('other')
+
+def test_square_tail():
+    assert ('square', 'X') == tail_translation('square')
+
+def test_squash_tail():
+    assert ('squash', 'X') == tail_translation('squash')
+
+def test_round_tail():
+    assert ('round', 'X') == tail_translation('round')
+
+def test_round_pin_tail():
+    assert ('round pin', 'X') == tail_translation('round pin')
+
+def test_pin_tail():
+    assert ('pin', 'X') == tail_translation('pin')
+
+def test_swallow_tail():
+    assert ('swallow', 'X') == tail_translation('swallow')
+
+def test_fish_tail():
+    assert ('fish', 'X') == tail_translation('fish')
+
+def test_other_tail():
+    assert ('tail', 'some other tail option') == tail_translation('some other tail option')
+
+def test_future_type():
+    assert ('future', 'X') == type_translation('future')
+
+def test_fcs_type():
+    assert ('fcstwo', 'X') == type_translation('fcs2')
+
+def test_fcs_type():
+    assert ('fcstwo', 'X') == type_translation('fcsii')
+
+def test_append_fin():
+    key_value_tuples = []
+    append_fin('fins', '2+1')
+    assert ('otherfin', 'X') in key_value_tuples
+
+def test_append_fin():
+    key_value_tuples = []
+    append_fin('fins', '2+1')
+    assert ('fins', '2+1') in key_value_tuples
