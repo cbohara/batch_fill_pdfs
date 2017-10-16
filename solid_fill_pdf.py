88019429dc1410a21abd6e7ee3a0655a2b8eb511
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-10-15 18:04:56 -0700

    no longer using id

--- solid_fill_pdf.py
+++ solid_fill_pdf.py
@@ -110,7 +110,6 @@ if __name__ == "__main__":
     try:
         pdf_template = sys.argv[1]
         csv_file = sys.argv[2]
-        input_id = sys.argv[3]
     except IndexError:
         print("python3 solid_fill_pdf.py pdf_template csv_file ")
     else:
@@ -123,9 +122,8 @@ if __name__ == "__main__":
                 os.makedirs(output_dir)
 
             for row in data:
-                if row[0][1] == input_id:
-                    orderid = row[3][1]
-                    output_path = os.path.join(output_dir, orderid)
-                    output_file = output_path + ".pdf"
-                    fill_pdf_template(row, pdf_template, output_file)
-                    print("Generated {0}.pdf".format(orderid))
+                orderid = row[3][1]
+                output_path = os.path.join(output_dir, orderid)
+                output_file = output_path + ".pdf"
+                fill_pdf_template(row, pdf_template, output_file)
+                print("Generated {0}.pdf".format(orderid))
